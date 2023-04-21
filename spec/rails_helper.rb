a91efa083ae0ef361be7493f52fefc1aa530d9e1
Captain <pongdanai@nimblehq.co>
2023-04-21 18:24:33 +0700

    [#2] Add simplecov-json in rails_helper to generate coverage json file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,10 @@
 require 'spec_helper'
 require 'simplecov'
 require 'simplecov-lcov'
+require 'simplecov-json'
 SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
 SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.formatter = SimpleCov::Formatter::JSONFormatter
 SimpleCov.start do
   add_filter(%r{/spec/}) # For RSpec
   enable_coverage(:branch) # Report branch coverage to trigger branch-level undercover warnings
